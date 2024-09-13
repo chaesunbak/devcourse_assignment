2bf2ef7fc81913c65f2deb3ee052d9f321868345
chaesunbak <coanstjd0422@gmail.com>
2024-09-13 20:28:42 +0900

    Create db-demo.js

---
+++ db-demo.js
@@ -0,0 +1,28 @@
+// Get the client
+const mysql = require("mysql2/promise");
+
+async function main() {
+  // Create the connection to database
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "Youtube",
+    dateStrings: true,
+  });
+
+  // A simple SELECT query
+  try {
+    const [results, fields] = await connection.query("SELECT * FROM users");
+
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await connection.end();
+  }
+}
+
+// Call the main function
+main();
