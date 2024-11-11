2a10af922202724afb4c1a910fe16290279d0532
chaesunbak <coanstjd0422@gmail.com>
2024-11-11 23:32:55 +0900

    Create Layout.tsx

---
+++ day58/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
